82ab567b16b3c67fcb5d7f9b6269e4d9e33c3012
dwedzikowski <44393596+dwedzikowski@users.noreply.github.com>
2018-12-11 06:06:08 +0900

    Update Kolorowanie.cpp
    Added some placeholder functions

--- Kolorowanie.cpp
+++ Kolorowanie.cpp
@@ -5,53 +5,89 @@
 #include "fstream"
 #include "string"
 
+#define POPULATION 100
+#define CROSS_CHANCE 50
+#define MUTATION_CHANCE 0
+#define STOP_MOMENT 15
+
 using namespace std;
 
+void fitness(int *tab, int size) {
+	int min = 99999999;
+	for (int i = 1; i > size; i++) {
+		if (tab[i] < min)
+			min = tab[i];
+	}
+	tab[0] = min;
+}
+void sort(int **tab, int M, int N) {
+	int temp = 0;
+	int min = 9999999;
+	int id = 0;
+	for (int i = 0; i < M; i++) {
+		for (int j = i; j < M; j++) {
+			if (tab[j][0] < min) {
+				min = tab[j][0];
+				id = j;
+			}
+		}
+		for (int j = 0; j < N; j++) {
+			temp = tab[i][j];
+			tab[i][j] = tab[id][j];
+			tab[id][j] = temp;
+		}
+		min = 99999999;
+	}
+}
+void cross(int **tab, int M, int N, int parent1, int parent2, int child) {
+	for (int i = 0; i < N / 2; i++) {
+		tab[child][i] = tab[parent1][i];
+	}
+	for (int i = (N / 2 + 1); i < N; i++) {
+		tab[child][i] = tab[parent2][i];
+	}
+}
+void clean(int **graph, int *tab, int N) {
+	
+}
 int GetFirstFreeColor(int *check) {					//checks for the first available color
 	int i = 1;
 	while (check[i])
 		i++;
 	return i;
 }
-void BruteForce(int **t, int *ord, int s) {				
+void Greedy(int **tab, int *order, int s, int *current) {
 	int* colors = new int[s + 1];
 	for (int i = 0; i <= s; i++)
 		colors[i] = 0;
-
-	/*
-	for (int i = 1; i <= s; i++) {
-		for (int j = 1; j <= s; j++) {			//Writing the graph's array onto the console
-			cout << t[i][j];
-		}
-		cout << '\n';
-	}
-	*/
+	
 
 	for (int i = 0; i < s; i++) {					// i - goes through the order array, j - checks vertice neighbours
 		for (int j = 1; j <= s; j++) {
-			if (t[ord[i]][j] && (ord[i] != j)) {		//checks if the edge between ord[i] and j vertice exists and if it's not checking an edge (a,a)
-				colors[t[j][j]] = 1;					//notes the colors of the "j" vertice
+			if (tab[order[i]][j] && (order[i] != j)) {		//checks if the edge between ord[i] and j vertice exists and if it's not checking an edge (a,a)
+				colors[tab[j][j]] = 1;					//notes the colors of the "j" vertice
 			}
 		}
-		t[ord[i]][ord[i]] = GetFirstFreeColor(colors);		//pushes the first available color for the "a" vertice onto the [a,a] spot in the array
+		
+		tab[order[i]][order[i]] = current[order[i]] = GetFirstFreeColor(colors);		//pushes the first available color for the "a" vertice onto the [a,a] spot in the array
 		for (int i = 0; i <= s; i++)						//resetting the colors array
-			colors[i] = 0;												
+			colors[i] = 0;
 	}
-	for (int i = 1; i <= s; i++) {							//writing the specific vertices' colors onto the console
-		cout << t[i][i] << " ";
+	for (int i = 0; i < s; i++) {							//writing the specific vertices' colors onto the console
+		cout << order[i] << " ";
 	}
 	cout << endl;
-	delete [] colors;
+	delete[] colors;
 }
 int main()
 {
 	string buf, buf1;
 	int V = 0, size = 0;
 	ifstream file;
-	
+
 	file.open("myciel4.txt");						//Opening the file
 
-	if (file.is_open()){
+	if (file.is_open()) {
 
 		getline(file, buf);
 		V = stoi(buf);
@@ -70,22 +106,36 @@ int main()
 			getline(file, buf, ' ');				//"Grabbing" the edge...
 			getline(file, buf1, '\n');
 
-			tab[stoi(buf)][stoi(buf1)] = 1;	
+			tab[stoi(buf)][stoi(buf1)] = 1;
 			tab[stoi(buf1)][stoi(buf)] = 1;				//...and slapping it into the array
 		}
-		
-		
-		int *order = new int[V];
-		for (int i = 0; i < V; i++)					//Temporary coloring order array (put permutation generator here)
-			order[i] = i + 1;
 
-		BruteForce(tab, order, V);					//Where the magic happens...
+		for (int i = 1; i <= V; i++) {
+			for (int j = 1; j <= V; j++) {			//Writing the graph's array onto the console
+				cout << tab[i][j];
+			}
+			cout << '\n';
+		}
+		int *order = new int[V];
 		
-		for (int i = 0; i < size; i++)
-			delete [] tab[i];
-		delete [] tab;
-    		delete [] order;
+		int **populus = new int*[POPULATION];
+		for (int i = 0; i < POPULATION; i++)
+			populus[i] = new int[size];
+		for (int i = 0; i < POPULATION - 1; i++)
+			populus[i][0] = 0;
+
+		for (int j = 0; j < V; j++)					//Temporary coloring order array (put permutation generator here)
+			order[j] = j + 1;
+
+		/*for (int i = 0; i < V; i++) {	
+			Greedy(tab, order, V, populus[i]);					//Where the magic happens...
+
+			
+		}
+		*/
+		
+		
 	}
-		
-    return 0;
+
+	return 0;
 }
